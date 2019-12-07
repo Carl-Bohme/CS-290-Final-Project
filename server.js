855e4829652f5c5b2d1f9d976d2a0a0e1f1e46b4
Carl-Bohme <55897133+Carl-Bohme@users.noreply.github.com>
2019-12-06 16:19:55 -0800

    Basic Templates

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var recipeData = require('./recipeData');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 
+app.get('/', function (req, res, next) {
+    res.status(200).render('homePage');
+});
+
 app.get('*', function (req, res) {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(port, function () {
